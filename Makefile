9993279f932ff3d94ce6fe1f8b8c993a9b8d0177
James Christie <james.aaron.christie@gmail.com>
2015-02-28 17:27:07 +0000

    Initial commit of blank project

---
+++ Makefile
@@ -0,0 +1,26 @@
+PYTHON=`which python`
+NAME=`python setup.py --name`
+
+all: init init-dev dev source
+
+dev: check test
+
+init:
+	pip install -r -requirements.txt
+
+init-dev:
+	pip install -r requirements-dev.txt
+
+test:
+	./pyspec.sh
+
+source:
+	$(PYTHON) setup.py sdist
+
+check:
+	find . -name \*.py | grep -v "tests/*test_*.py" | xargs pylint --errors-only --reports=n
+
+clean:
+	$(PYTHON) setup.py clean
+	rm -rf build/ MANIFEST dist build libmorris.egg-info
+	find . -name '*.pyc' -delete
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+  name='libmorris',
+  version='1.0.0',
+  author='James Christie',
+  author_email='james.aaron.christie@gmail.com',
+  url='http://github.com/JamesChristie/libmorris',
+  download_url='https://github.com/JamesChristie/libmorris.git',
+  description='A library implementation of tic-tac-toe',
+  long_description='A library implementation of tic-tac-toe',
+
+  packages = find_packages(),
+  include_package_data = True,
+  package_data = {
+    '': ['*.txt', '*.rst'],
+    'libmorris': ['credentials', 'client'],
+  },
+  exclude_package_data = { '': ['README.txt'] },
+  
+  keywords='python rest netsuite erp',
+  license='GPL',
+  classifiers=['Development Status :: 5 - Production/Stable',
+               'Natural Language :: English',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python :: 3',
+               'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+               'License :: OSI Approved :: GNU Affero General Public License v3',
+               'Topic :: Internet',
+               'Topic :: Internet :: WWW/HTTP',
+              ],
+              
+  install_requires = ['setuptools'],
+)
